fee6a29b454dda755860175aebfe68053026aa8f
Valentin Vadász <vadaszvalentin@gmail.com>
2023-03-01 17:03:14 +0100

    Movie repository

---
+++ Movies.Application/Repositories/IMovieRepository.cs
@@ -0,0 +1,13 @@
+﻿using Movies.Application.Models;
+
+namespace Movies.Application.Repositories
+{
+    internal interface IMovieRepository
+    {
+        public Task<bool> CreateAsync(Movie movie);
+        public Task<Movie?> GetByIdAsync(Guid id);
+        public Task<IEnumerable<Movie>> GetAllAsync();
+        public Task<bool> UpdateAsync(Movie movie);
+        public Task<bool> DeleteAsync(Guid id);
+    }
+}
