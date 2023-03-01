9c188aca07532c2781f6312b35309904bd5bac4e
Valentin Vadász <vadaszvalentin@gmail.com>
2023-03-01 16:39:51 +0100

    Movie response and request contracts

---
+++ Movies.Contracts/Responses/MovieResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Movies.Contracts.Responses
+{
+    internal class MovieResponse
+    {
+        public required Guid Id { get; init; }
+        public required string Name { get; init; }
+        public required int YearOfRelease { get; init; }
+        public required IEnumerable<string> Genres { get; init; } = Enumerable.Empty<string>();
+    }
+}
