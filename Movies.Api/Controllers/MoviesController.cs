9ff628d9b93058d913df24e2606dc03443888250
Valentin Vadász <vadaszvalentin@gmail.com>
2023-03-01 22:24:17 +0100

    Add movies controller

---
+++ Movies.Api/Controllers/MoviesController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Movies.Application.Models;
+using Movies.Application.Repositories;
+using Movies.Contracts.Requests;
+
+namespace Movies.Api.Controllers
+{
+    [ApiController]
+    [Route("api/movies")]
+    public class MoviesController : ControllerBase
+    {
+        private readonly IMovieRepository _movieRepository;
+
+        public MoviesController(IMovieRepository movieRepository)
+        {
+            this._movieRepository = movieRepository;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] CreateMovieRequest createRequest)
+        {
+            var movie = new Movie
+            {
+                Id = Guid.NewGuid(),
+                Name = createRequest.Name,
+                YearOfRelease = createRequest.YearOfRelease,
+                Genres = createRequest.Genres.ToList()
+            };
+
+            await _movieRepository.CreateAsync(movie);
+
+            return Created($"api/movies/{movie.Id}", movie);
+        }
+    }
+}
